d0ced041dd84d4b49375929e3c72f80cb5d0a044
SotaYamaguchi <sota.yamaguchi@roxx.co.jp>
2023-12-03 22:49:40 +0900

    Initial commit

---
+++ test/sqs-fargate-ddb-typescript.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SqsFargateDdbTypescript from '../lib/sqs-fargate-ddb-typescript-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sqs-fargate-ddb-typescript-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SqsFargateDdbTypescript.SqsFargateDdbTypescriptStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
